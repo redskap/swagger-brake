02b56193f40fa7007ca98d57102754175cbb83a1
Arnold Galovics <galovicsarnold@gmail.com>
2018-10-27 19:27:50 +0200

    Support for required parameter detection

--- src/main/java/com/arnoldgalovics/blog/swagger/breaker/core/model/RequestParameter.java
+++ src/main/java/com/arnoldgalovics/blog/swagger/breaker/core/model/RequestParameter.java
@@ -13,14 +13,16 @@ public class RequestParameter {
     private final RequestParameterInType inType;
     private final String name;
     private final Schema schema;
+    private final boolean required;
 
-    public RequestParameter(RequestParameterInType inType, String name) {
-        this(inType, name, null);
+    public RequestParameter(RequestParameterInType inType, String name, boolean required) {
+        this(inType, name, required, null);
     }
 
-    public RequestParameter(RequestParameterInType inType, String name, Schema schema) {
+    public RequestParameter(RequestParameterInType inType, String name, boolean required, Schema schema) {
         this.inType = inType;
         this.name = name;
+        this.required = required;
         this.schema = schema;
     }
 
--- src/main/java/com/arnoldgalovics/blog/swagger/breaker/core/model/transformer/ParameterTransformer.java
+++ src/main/java/com/arnoldgalovics/blog/swagger/breaker/core/model/transformer/ParameterTransformer.java
@@ -6,6 +6,7 @@ import com.arnoldgalovics.blog.swagger.breaker.core.model.service.RequestParamet
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,10 +19,11 @@ public class ParameterTransformer implements Transformer<Parameter, RequestParam
     public RequestParameter transform(Parameter from) {
         RequestParameterInType inType = requestParameterInTypeResolver.resolve(from.getIn());
         String name = from.getName();
+        boolean required = BooleanUtils.toBoolean(from.getRequired());
         Schema swSchema = from.getSchema();
         if (swSchema != null) {
-            return new RequestParameter(inType, name, schemaTransformer.transform(swSchema));
+            return new RequestParameter(inType, name, required, schemaTransformer.transform(swSchema));
         }
-        return new RequestParameter(inType, name);
+        return new RequestParameter(inType, name, required);
     }
 }
