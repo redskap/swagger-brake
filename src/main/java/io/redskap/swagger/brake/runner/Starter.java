b829ff6aaa9ff124064bd633a38f698e31596647
Arnold Galovics <galovicsarnold@gmail.com>
2018-12-11 20:26:45 +0100

    #11 Starter should return results

--- src/main/java/io/redskap/swagger/brake/runner/Starter.java
+++ src/main/java/io/redskap/swagger/brake/runner/Starter.java
@@ -1,14 +1,17 @@
 package io.redskap.swagger.brake.runner;
 
+import java.util.Collection;
+
+import io.redskap.swagger.brake.core.BreakingChange;
 import io.redskap.swagger.brake.core.CoreConfiguration;
 import io.redskap.swagger.brake.maven.MavenConfiguration;
 import io.redskap.swagger.brake.report.ReporterConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Starter {
-    public static void start(Options options) {
+    public static Collection<BreakingChange> start(Options options) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RunnerConfiguration.class, ReporterConfiguration.class,
             MavenConfiguration.class, CoreConfiguration.class);
-        context.getBean(Runner.class).run(options);
+        return context.getBean(Runner.class).run(options);
     }
 }
