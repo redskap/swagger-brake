e3130eb2a201aff0757ad53aa5cfc4e51c7c7ff0
Arnold Galovics <galovicsarnold@gmail.com>
2020-04-15 20:41:58 +0200

    #27 Exiting with code 1 in case of breaking changes or error

--- swagger-brake-cli/src/main/java/io/redskap/swagger/brake/cli/Cli.java
+++ swagger-brake-cli/src/main/java/io/redskap/swagger/brake/cli/Cli.java
@@ -1,30 +1,40 @@
 package io.redskap.swagger.brake.cli;
 
+import java.util.Collection;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.redskap.swagger.brake.cli.options.CliHelpException;
 import io.redskap.swagger.brake.cli.options.CliOptions;
 import io.redskap.swagger.brake.cli.options.CliOptionsProvider;
+import io.redskap.swagger.brake.core.BreakingChange;
 import io.redskap.swagger.brake.runner.Options;
 import io.redskap.swagger.brake.runner.Starter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@SuppressFBWarnings("DM_EXIT")
 public class Cli {
     private final CliOptionsProvider optionsProvider;
 
     public void start() {
         try {
             Options options = optionsProvider.provide();
-            Starter.start(options);
+            Collection<BreakingChange> breakingChanges = Starter.start(options);
+            if (CollectionUtils.isNotEmpty(breakingChanges)) {
+                System.exit(1);
+            }
         } catch (CliHelpException e) {
             log.info(e.getMessage());
         } catch (Exception e) {
             log.debug("Exception occured", e);
             log.error(e.getMessage());
             log.error("For help please use " + CliOptions.getAsCliOption(CliOptions.HELP));
+            System.exit(1);
         }
     }
 }
