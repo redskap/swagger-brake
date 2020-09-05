c832f5b6e8dc74ad9d51e21f7b5c355e9df2aac8
Dylan Watson <lotsabackscatter@gmail.com>
2020-09-06 00:10:31 +0800

    Add support for release field in MavenMetadata (#38)
    * Add a release field to MavenVersioning
    
    This matches the XML model generated by Nexus:
    <metadata>
      <versioning>
        <release>1.0.567</release>
      </versioning>
    </metadata>
    
    * Attempt to use release as the latest artifact version

--- swagger-brake/src/main/java/io/redskap/swagger/brake/maven/model/MavenVersioning.java
+++ swagger-brake/src/main/java/io/redskap/swagger/brake/maven/model/MavenVersioning.java
@@ -11,5 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MavenVersioning {
     private String latest;
+    private String release;
     private MavenSnapshot snapshot;
 }
