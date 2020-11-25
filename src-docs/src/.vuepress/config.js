a2c5f8400d9e9e6941b17c141ccfd25a8ede2ea6
Arnold Galovics <galovicsarnold@gmail.com>
2020-11-25 09:29:53 +0100

    Moved from measurement id to universal id

--- src-docs/src/.vuepress/config.js
+++ src-docs/src/.vuepress/config.js
@@ -32,6 +32,6 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-    ['@vuepress/plugin-google-analytics', { ga: "G-XDJP4RYYJ6" }],
+    ['@vuepress/plugin-google-analytics', { ga: "UA-78900346-2" }],
   ]
 }
