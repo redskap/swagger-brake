1fdd48c8f0d1b1d3d4be239b5729f8027d559377
Arnold Galovics <galovicsarnold@gmail.com>
2023-10-04 11:13:36 +0200

    Upgraded integration tests to use Swagger Brake 2.4.0

--- swagger-brake-integration-tests/src/test/java/io/redskap/swagger/brake/integration/support/SwaggerBrakeVersion.java
+++ swagger-brake-integration-tests/src/test/java/io/redskap/swagger/brake/integration/support/SwaggerBrakeVersion.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class SwaggerBrakeVersion {
-    public static final SwaggerBrakeVersion V_1_1_0 = new SwaggerBrakeVersion("1.1.0");
-    public static final SwaggerBrakeVersion V_2_0_0 = new SwaggerBrakeVersion("2.0.0");
-    public static final SwaggerBrakeVersion V_2_1_0 = new SwaggerBrakeVersion("2.1.0");
-    public static final SwaggerBrakeVersion V_2_2_0 = new SwaggerBrakeVersion("2.2.0");
-    public static final SwaggerBrakeVersion V_2_3_0 = new SwaggerBrakeVersion("2.3.0");
-    public static final SwaggerBrakeVersion V_LATEST_VERSION = new SwaggerBrakeVersion("2.3.0");
+    public static final SwaggerBrakeVersion V_LATEST_VERSION = new SwaggerBrakeVersion("2.4.0");
 
     private final String version;
 
