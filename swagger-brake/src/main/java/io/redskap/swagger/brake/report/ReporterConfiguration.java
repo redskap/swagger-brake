b14443e7f38de1b398044464615f5a2e8d491e03
Arnold Galovics <galovicsarnold@gmail.com>
2024-05-15 11:13:25 +0200

    Fix for dep upgrades

--- swagger-brake/src/main/java/io/redskap/swagger/brake/report/ReporterConfiguration.java
+++ swagger-brake/src/main/java/io/redskap/swagger/brake/report/ReporterConfiguration.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan
 public class ReporterConfiguration {
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
