bc0bf7cd8b4bc1136ee4759af5f807eba67b880d
Arnold Galovics <galovicsarnold@gmail.com>
2025-10-15 14:21:10 +0200

    Further upgrades, including Java21 upgrade

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 # Build
-FROM amazoncorretto:17-alpine-jdk AS baseimage
+FROM amazoncorretto:21-alpine-jdk AS baseimage
 
 WORKDIR swagger-brake
 COPY . .
 RUN ./gradlew --no-daemon clean build shadowJar -x test
 
 # Actual container
-FROM amazoncorretto:17-alpine-jdk
+FROM amazoncorretto:21-alpine-jdk
 WORKDIR swagger-brake
 COPY --from=baseimage /swagger-brake/swagger-brake-cli/build/libs/swagger-brake-*-cli.jar swagger-brake.jar
 
--- swagger-brake/src/test/java/io/redskap/swagger/brake/integration/v2/response/RecursiveTypeAttributeRemovedIntTest.java
+++ swagger-brake/src/test/java/io/redskap/swagger/brake/integration/v2/response/RecursiveTypeAttributeRemovedIntTest.java
@@ -2,13 +2,12 @@ package io.redskap.swagger.brake.integration.v2.response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Collection;
-import java.util.Collections;
-
 import io.redskap.swagger.brake.core.BreakingChange;
 import io.redskap.swagger.brake.core.model.HttpMethod;
 import io.redskap.swagger.brake.core.rule.response.ResponseTypeAttributeRemovedBreakingChange;
 import io.redskap.swagger.brake.integration.AbstractSwaggerBrakeIntTest;
+import java.util.Collection;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,11 +21,13 @@ public class RecursiveTypeAttributeRemovedIntTest extends AbstractSwaggerBrakeIn
         String newApiPath = "swaggers/v2/response/recursiveresponseattributeremoved/schema_v2.json";
         ResponseTypeAttributeRemovedBreakingChange bc1 =
             new ResponseTypeAttributeRemovedBreakingChange("/api/v1/audits/summary/{businessId}", HttpMethod.GET, "200", "unverifiedPayoffBreakdown.amountApplied");
-        Collection<BreakingChange> expected = Collections.singletonList(bc1);
+        ResponseTypeAttributeRemovedBreakingChange bc2 =
+            new ResponseTypeAttributeRemovedBreakingChange("/api/v1/audits/summary/{businessId}", HttpMethod.GET, "200", "unverifiedPayoffBreakdown.children.amountApplied");
+        Collection<BreakingChange> expected = List.of(bc1, bc2);
         // when
         Collection<BreakingChange> result = execute(oldApiPath, newApiPath);
         // then
-        assertThat(result).hasSize(1);
+        assertThat(result).hasSize(2);
         assertThat(result).hasSameElementsAs(expected);
     }
 }
